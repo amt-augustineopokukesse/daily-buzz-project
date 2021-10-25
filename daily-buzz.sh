5e92351838fc631113154f5001ae601c7958fbcd
Augustine Opoku-Kesse <augustine.opoku-kesse@amalitech.org>
2021-10-25 21:57:02 +0100

    Daily buzz project file included

---
+++ daily-buzz.sh
@@ -0,0 +1,37 @@
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ ls
+culture  health  national  technology
+$ cd national/politics
+$ ls
+policy
+$ mkdir elections
+$ cd elections
+$ pwd
+/home/ccuser/workspace/daily-buzz/national/politics/elections
+$ touch candidates.txt
+$ touch issues.txt
+$ cd ../../..
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ 
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ mkdir business
+$ cd business
+$ ls
+$ mkdir startups
+$ cd startups
+$ pwd
+/home/ccuser/workspace/daily-buzz/business/startups
+$ cd ..
+$ mkdir startups/disruptors
+$ pwd
+/home/ccuser/workspace/daily-buzz/business
+$ touch startups/disruptors/tech.txt
+$ touch startups/disruptors/design.txt
+$ touch startups/disruptors/education.txt
+$ cd ..
+$ pwd
+/home/ccuser/workspace/daily-buzz
+$ 
